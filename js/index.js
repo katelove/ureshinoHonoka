b46c2dbfd321b3afd44eab150b3f810b30a7e45a
Kate <katelovejesus1110@gmail.com>
2021-08-10 15:15:23 +0800

    add close icon

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 //打開menu 選單
 const menu=document.getElementById('menu-point')
 const menuList=document.getElementById("menu-ul")
+const menuClose=document.getElementById("menu-close")
 
 menu.addEventListener('click',function(){
-    menuList.classList.toggle('hide')
+    menuList.classList.toggle('hide');
+    menuClose.style.display='block';
+})
+
+menuClose.addEventListener('click',function(){
+    menuList.classList.toggle('hide');
+    menuClose.style.display='none';
 })
